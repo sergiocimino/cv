56419e69ea36efd1865cddba69d98915d406dc8c
Sergio Cimino <scimino@gmail.com>
2023-03-07 22:35:39 -0300

    Primer Escritura en repo local

---
+++ js/funciones.js
@@ -0,0 +1,18 @@
+function mostrarPremio() {
+    document.getElementById('premio').style.display = 'block';
+    document.getElementById('triste').style.display = 'none';
+}
+
+function ocultarPremio() {
+    document.getElementById('premio').style.display = 'none';
+    document.getElementById('triste').style.display = 'block';
+}
+
+function verContacto(){
+    document.getElementById('contacto').style.display = 'block';
+}
+
+function ocultarContacto() {
+    document.getElementById('contacto').style.display = 'none';
+
+}
