52452dcb63f6b62762155a56691542423ba94f59
Sergio Cimino <scimino@gmail.com>
2023-03-07 23:23:52 -0300

    Cambio en funciones

--- js/funciones.js
+++ js/funciones.js
@@ -1,13 +1,3 @@
-function mostrarPremio() {
-    document.getElementById('premio').style.display = 'block';
-    document.getElementById('triste').style.display = 'none';
-}
-
-function ocultarPremio() {
-    document.getElementById('premio').style.display = 'none';
-    document.getElementById('triste').style.display = 'block';
-}
-
 function verContacto(){
     document.getElementById('contacto').style.display = 'block';
 }
@@ -16,3 +6,4 @@ function ocultarContacto() {
     document.getElementById('contacto').style.display = 'none';
 
 }
+
